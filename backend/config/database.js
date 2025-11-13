e066a600d07547dab02d697d0a326ee4483bf75c
DzahwanMalik <malikdzahwan22@gmail.com>
2025-11-13 21:13:59 +0700

    fix: enviroments variable database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,9 +3,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize("pemilihan_istar_putri_db", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
+const db = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
+  }
+);
 
 export default db;
