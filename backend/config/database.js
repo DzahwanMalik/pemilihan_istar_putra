396ec4338d3f18bcfd9009f0cc801b63b925d2a2
DzahwanMalik <malikdzahwan22@gmail.com>
2025-11-13 21:07:57 +0700

    fix: ui/reportPage, login system, update candidate

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,14 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const db = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql",
-  }
-);
+const db = new Sequelize("pemilihan_istar_putri_db", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
+});
 
 export default db;
